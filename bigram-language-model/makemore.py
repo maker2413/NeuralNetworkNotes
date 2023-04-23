a0dde93d866e3620734d9f67d09cf2bdc41eb2e3
maker2413 <maker2413@gmail.com>
2023-04-23 17:06:46 -0500

    setup pytorch for next steps of makemore

--- bigram-language-model/makemore.py
+++ bigram-language-model/makemore.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+import torch
+
+# Let's start by putting all of the words in our names.txt in a variable
 words = open('names.txt', 'r').read().splitlines()
-print(words[:10])
-print(len(words))
+
+# b will be our bigram dictionary
+b = {}
+# Let's iterate over all of the words we have
+for w in words:
+    # And let's add an imaginary start and end character to each of our words
+    chs = ['<S>'] + list(w) + ['<E>']
+    # Then we will iterate over each 2 character chunks of each word
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        # And add a count of each occurance of a 2 character pair to our
+        # dictionary
+        b[bigram] = b.get(bigram, 0) + 1
+
+print(sorted(b.items(), key = lambda kv: -kv[1]))
