0516957c5a94104141f563e8304544425206cb0d
maker2413 <maker2413@gmail.com>
2023-05-11 00:57:56 -0500

    implemented Value class

--- gradients/gradients.py
+++ gradients/gradients.py
@@ -4,6 +4,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from graphviz import Digraph
 
 def f(x):
     return 3*x**2 - 4*x + 5
@@ -74,3 +75,75 @@ d2 = a*b + c
 print('d1:', d1)
 print('d2:', d2)
 print('slope:', (d2 - d1)/h)
+
+# trace pieces together all of the nodes in our math problems
+def trace(root):
+    # builds a set of all nodes and edges in a graph
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+# draw_dot is used to draw a digram of our math problems from a root node
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        # for any value in the graph, create a rectangular ('record') node for it
+        dot.node(
+            name = uid, label = "{ %s | data %.4f | grad %.4f }" % (
+                n.label,
+                n.data,
+                n.grad
+            ),
+            shape='record'
+        )
+        if n._op:
+            # if this value is a result of some operation, create an op node for it
+            dot.node(name = uid + n._op, label = n._op)
+            # and connect this node to it
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        # connect n1 to the op node of n2
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+
+# Our Value class implements logic similar to a Tensor class found in PyTorch
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+        return out
