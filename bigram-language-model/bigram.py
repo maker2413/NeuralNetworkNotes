3f5a014edfe8ecd13fb51c6fd05de1407ad9b6bb
maker2413 <maker2413@gmail.com>
2023-05-22 15:46:09 -0500

    completed bigram language model

--- bigram-language-model/bigram.py
+++ bigram-language-model/bigram.py
@@ -98,3 +98,133 @@ for w in words[:1]:
         ix2 = stoi[ch2]
         xs.append(ix1)
         ys.append(ix2)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+print(xs)
+print(ys)
+
+import torch.nn.functional as F
+
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g)
+
+xenc = F.one_hot(xs, num_classes=27).float() # input to the net: one-hot encoding
+logits = xenc @ W # predict log-counts
+counts = logits.exp() # counts is equivalent to N
+probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+
+nlls = torch.zeros(5)
+for i in range(5):
+    # i-th bigram:
+    x = xs[i].item() # input character index
+    y = ys[i].item() # label character index
+    print('----------')
+    print(f'bigram example {i+1}: {itos[int(x)]}{itos[int(y)]} (indexes {x},{y})')
+    print('input to the neural net:', x)
+    print('output probabilities from the neural net:', probs[i])
+    print('label (actual next character):', y)
+    p = probs[i, y]
+    print('probability assigned by the net to the correct character:', p.item())
+    logp = torch.log(p)
+    print('log likelihood:', logp.item())
+    nll = -logp
+    print('negative log likelihood:', nll.item())
+    nlls[i] = nll
+
+print('=========')
+print('average negative log likelihood, i.e. loss =', nlls.mean().item())
+
+xs, ys = [], []
+
+for w in words[:1]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        xs.append(ix1)
+        ys.append(ix2)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+print('number of examples:', num)
+
+# randomly initialize 27 neurons' weights. Each neuron receives 27 inputs
+g = torch.Generator().manual_seed(2147483647)
+# requires_grad = True is required to tell PyTorch we want gradients
+W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+xenc = F.one_hot(xs, num_classes=27).float() # input to the net: one-hot encoding
+logits = xenc @ W # predict log-counts
+counts = logits.exp() # counts is equivalent to N
+probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+
+loss = -probs[torch.arange(num), ys].log().mean()
+loss.item()
+
+W.grad = None # set the gradient to zero
+loss.backward()
+
+W.data += -0.1 * W.grad
+
+xenc = F.one_hot(xs, num_classes=27).float() # input to the net: one-hot encoding
+logits = xenc @ W # predict log-counts
+counts = logits.exp() # counts is equivalent to N
+probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+loss = -probs[torch.arange(num), ys].log().mean()
+loss.item()
+
+xs, ys = [], []
+
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        xs.append(ix1)
+        ys.append(ix2)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+print('number of examples: ', num)
+
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+# gradient descent
+print('back propagating nn...')
+for k in range(100):
+    # forward pass
+    xenc = F.one_hot(xs, num_classes=27).float() # input to the net: one-hot encoding
+    logits = xenc @ W # predict log-counts
+    counts = logits.exp() # counts is equivalent to N
+    probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+    loss = -probs[torch.arange(num), ys].log().mean()
+    # print(loss.item())
+
+    # backward pass
+    W.grad = None # set to zero the gradient
+    loss.backward()
+
+    # update
+    W.data += -50 * W.grad
+
+print(loss.item())
+
+g = torch.Generator().manual_seed(2147483647)
+
+for i in range(5):
+    out = []
+    ix = 0
+    while True:
+        xenc = F.one_hot(torch.tensor([ix]), num_classes=27).float()
+        logits = xenc @ W
+        counts = logits.exp()
+        p = counts / counts.sum(1, keepdims=True)
+
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[int(ix)])
+        if ix == 0:
+            break
+    print(''.join(out[:(len(out) - 1)]))
