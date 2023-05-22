09b1ac4132d2cb4bbdd8fee45f63859792c2ebda
maker2413 <maker2413@gmail.com>
2023-05-22 16:05:31 -0500

    added requirements.txt for mlp section

--- multilayer-perceptron/mlp.py
+++ multilayer-perceptron/mlp.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python
 # This file was generated from the code blocks in ./README.org.
+
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt # for making figures
