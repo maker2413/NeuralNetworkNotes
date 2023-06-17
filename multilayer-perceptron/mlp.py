a635c6fa1265e8f3bcca027c344db7eb30d91c1e
maker2413 <maker2413@gmail.com>
2023-06-17 14:52:48 -0500

    A little more progress on MLP notes

--- multilayer-perceptron/mlp.py
+++ multilayer-perceptron/mlp.py
@@ -4,3 +4,10 @@
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt # for making figures
+
+words = open('names.txt', 'r').read().splitlines()
+
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
