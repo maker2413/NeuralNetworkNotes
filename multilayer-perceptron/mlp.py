a9523386c062800b8d78c738f5d2eaad6c52b456
maker2413 <maker2413@gmail.com>
2024-11-10 12:44:17 -0600

    making progress on multilayer perceptrons

--- multilayer-perceptron/mlp.py
+++ multilayer-perceptron/mlp.py
@@ -11,3 +11,29 @@ chars = sorted(list(set(''.join(words))))
 stoi = {s:i+1 for i,s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
+
+# Build the dataset
+block_size = 3 # context length: how many characters do we take to predict the next one
+X, Y = [], []
+for w in words[:5]:
+    print(w)
+    context = [0] * block_size
+    for ch in w + '.':
+        ix = stoi[ch]
+        X.append(context)
+        Y.append(ix)
+        print(''.join(itos[i] for i in context), '--->', itos[ix])
+        context = context[1:] + [ix] # crop and append
+
+X = torch.tensor(X)
+Y = torch.tensor(Y)
+
+C = torch.randn((27, 2))
+
+emb = C[X]
+emb.shape
+
+W1 = torch.randn((6, 100))
+b1 = torch.randn(100)
+
+h = emb.view(32, 6) @ W1 + b1
