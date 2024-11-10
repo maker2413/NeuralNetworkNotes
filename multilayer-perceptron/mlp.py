a54a92c22ecdf50f291c1f487fa68a1275e472d0
maker2413 <maker2413@gmail.com>
2024-11-10 15:43:22 -0600

    at a good stopping point for mlp

--- multilayer-perceptron/mlp.py
+++ multilayer-perceptron/mlp.py
@@ -50,3 +50,21 @@ loss = -prob[torch.arange(32), Y].log().mean()
 loss
 
 # ========================= now made respectable =========================
+
+g = torch.Generator().manual_seed(2147483647) # for reproducibility
+C = torch.randn((27, 2), generator=g)
+W1 = torch.randn((6, 100), generator=g)
+b1 = torch.randn(100, generator=g)
+W2 = torch.randn((100, 27), generator=g)
+b2 = torch.randn(27, generator=g)
+
+parameters = [C, W1, b1, W2, b2]
+
+emb = C[X] # (32, 3, 2)
+h = torch.tanh(emb.view(-1, 6) @ W1 + b1) # (32, 100)
+logits = h @ W2 + b2 # (32, 27)
+counts = logits.exp()
+prob = counts / counts.sum(1, keepdims=True)
+loss = -prob[torch.arange(32), Y].log().mean()
+
+loss
